4a3031b04f9c430299d3ecc0e0147a381c26ab7e
konigin144 <kinga777s@gmail.com>
2021-11-14 13:46:58 +0100

    First draft
    Grids, WrapPanels

--- ComeX.WPF/MainWindow.xaml.cs
+++ ComeX.WPF/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,16 +14,48 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ComeX.WPF
-{
+namespace ComeX.WPF {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+    public partial class MainWindow : Window {
+        public MainWindow() {
             InitializeComponent();
+
+            for (int i = 0; i < 3; i++) {
+                Button newBtn = new Button();
+
+                newBtn.Content = i.ToString();
+                newBtn.Name = "Button" + i.ToString();
+
+                ServersWrapP.Children.Insert(0, newBtn);
+            }
+
+            
+        }
+
+        private void SendMessage(object sender, RoutedEventArgs e) {
+            if (TypeTextBox.Text != "") {
+                MessageUserControl newMessage = new MessageUserControl { };
+                newMessage.AuthorText.Text = "Anonim";
+                newMessage.DateText.Text = DateTime.Now.ToString();
+                newMessage.ContentText.Text = TypeTextBox.Text;
+                MessagesWrapP.Children.Add(newMessage);
+
+                TypeTextBox.Text = "";
+            }
+        }
+
+        private void AddMessagePlaceholder(object sender, RoutedEventArgs e) {
+            TextBox textbox = sender as TextBox;
+            if (textbox.Text == "")
+                textbox.Text = "Type message here...";
+        }
+
+        private void RemoveMessagePlaceholder(object sender, RoutedEventArgs e) {
+            TextBox textbox = sender as TextBox;
+            if (textbox.Text == "Type message here...")
+                textbox.Text = "";
         }
     }
 }
---
+++ ComeX.WPF/MessageUserControl.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ComeX.WPF {
+    /// <summary>
+    /// Logika interakcji dla klasy MessageUserControl.xaml
+    /// </summary>
+    public partial class MessageUserControl : UserControl {
+        public MessageUserControl() {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty ContentProperty = DependencyProperty.Register("Content", typeof(string), typeof(MessageUserControl));
+        public static readonly DependencyProperty AuthorProperty = DependencyProperty.Register("Author", typeof(string), typeof(MessageUserControl));
+        public static readonly DependencyProperty DateProperty = DependencyProperty.Register("Date", typeof(string), typeof(MessageUserControl));
+
+    }
+}
