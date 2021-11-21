43da852f389a04d4c2fd8d787ab16e150a937520
ElStempel <steplewski.aleks@gmail.com>
2021-11-21 20:43:42 +0100

    podwaliny po ustaleniach

--- ComeX.Server/Hubs/ComeXChatHub.cs
+++ ComeX.Server/Hubs/ComeXChatHub.cs
@@ -8,9 +8,29 @@ namespace ComeX.Server.Hubs
 {
     public class ComeXChatHub : Hub
     {
+        // otrzymano wiadomość tekstową
         public async Task SendChatMessage()
         {
+            // check czy ma plik
             await Clients.All.SendAsync("RecieveChatMessage");
+        }
+
+        // otrzymano ankietę
+        public async Task SendChatSurvey()
+        {
+            await Clients.All.SendAsync("RecieveChatSurvey");
+        }
+
+        // otrzymano odpowiedzi do ankiety
+        public async Task SendChatSurveyAnswer()
+        {
+            await Clients.All.SendAsync("RecieveChatSurveyAnswer");
+        }
+
+        // otrzymano głos do ankiety
+        public async Task SendChatSurveyVote()
+        {
+            await Clients.All.SendAsync("RecieveChatSurveyVote");
         }
     }
 }
