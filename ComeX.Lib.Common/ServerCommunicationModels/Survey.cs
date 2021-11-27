b8e67ee4f2649d72678e6bddb0d1f60c54deede9
konigin144 <kinga777s@gmail.com>
2021-11-27 21:14:24 +0100

    Communications models

---
+++ ComeX.Lib.Common/ServerCommunicationModels/Survey.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ComeX.Lib.Common.ServerCommunicationModels {
+    public class Survey {
+        public Survey(string token, Guid roomId, string question, bool isMultipleChoice) {
+            Token = token;
+            RoomId = roomId;
+            Question = question;
+            IsMultipleChoice = isMultipleChoice;
+        }
+
+        [JsonProperty("Token")]
+        public string Token { get; set; }
+        [JsonProperty("RoomId")]
+        public Guid RoomId { get; set; }
+        [JsonProperty("Question")]
+        public string Question { get; set; }
+        [JsonProperty("IsMultipleChoice")]
+        public bool IsMultipleChoice { get; set; }
+    }
+}
