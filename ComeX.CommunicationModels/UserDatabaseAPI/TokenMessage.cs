12001dc896a1ececff3935e38aaede5396ed190b
Dzemoro <dellorpl@gmail.com>
2021-11-27 16:56:44 +0100

    Changed name of library

--- ComeX.CommunicationModels/UserDatabaseAPI/TokenMessage.cs
+++ ComeX.CommunicationModels/UserDatabaseAPI/TokenMessage.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
-namespace ComeX.CommunicationModels.UserDatabaseAPI
+namespace ComeX.Lib.Common.UserDatabaseAPI
 {
     public class TokenMessage
     {
