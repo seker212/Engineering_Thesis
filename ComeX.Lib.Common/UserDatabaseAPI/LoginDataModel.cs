e54916c79a5e372187f2ae3252f7241d0161de82
Dzemoro <dellorpl@gmail.com>
2021-11-27 21:13:29 +0100

    Added LoginDataModel

---
+++ ComeX.Lib.Common/UserDatabaseAPI/LoginDataModel.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComeX.Lib.Common.UserDatabaseAPI
+{
+    public class LoginDataModel
+    {
+        public LoginDataModel(string token, string username)
+        {
+            Token = token;
+            Username = username;
+        }
+
+        [JsonProperty("Token")]
+        public string Token { get; set; }
+        [JsonProperty("Username")]
+        public string Username { get; set; }
+    }
+}
