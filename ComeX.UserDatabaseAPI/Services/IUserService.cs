e66c03fd585fdcf918a51cea55632c1b62e20e6c
Dzemoro <dellorpl@gmail.com>
2021-12-11 15:10:13 +0100

    Added chech if user with given username already exists

--- ComeX.UserDatabaseAPI/Services/IUserService.cs
+++ ComeX.UserDatabaseAPI/Services/IUserService.cs
@@ -6,7 +6,7 @@ namespace ComeX.UserDatabaseAPI.Services
 {
     public interface IUserService
     {
-        Task<User> CreateUser(string username, string password);
+        Task<(string Reason, User AddedUser)> CreateUser(string username, string password);
         Task<Lib.Common.UserDatabaseAPI.LoginDataModel> Login(string username, string password);
         Task<User> UpdateUser(string username, string password, string newPassword);
         Task<bool> DeleteUser(string username, string password);
