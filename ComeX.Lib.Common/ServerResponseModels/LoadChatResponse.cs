5c863f8f24fcc0ce8c857f3ede944c1773bf089a
konigin144 <kinga777s@gmail.com>
2022-01-02 17:35:06 +0100

    Load message - request and response

--- ComeX.Lib.Common/ServerResponseModels/LoadChatResponse.cs
+++ ComeX.Lib.Common/ServerResponseModels/LoadChatResponse.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ComeX.Lib.Common.ServerResponseModels {
-    class LoadChatResponse {
+    public class LoadChatResponse {
+        public LoadChatResponse(Guid roomId, List<MessageResponse> messageList) {
+            RoomId = roomId;
+            MessageList = messageList;
+        }
+
+        [JsonProperty("RoomId")]
+        public Guid RoomId { get; set; }
+        [JsonProperty("MessageList")]
+        public List<MessageResponse> MessageList { get; set; }
     }
 }
---
+++ ComeX.Lib.Common/ServerResponseModels/LoadMessageResponse.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComeX.Lib.Common.ServerResponseModels {
+    public class LoadMessageResponse {
+        public LoadMessageResponse(MessageResponse message) {
+            Message = message;
+        }
+
+        [JsonProperty("Message")]
+        public MessageResponse Message { get; set; }
+    }
+}
