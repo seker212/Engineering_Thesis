fbc9d924dd38db261536d30f95c202bff2141e27
konigin144 <kinga777s@gmail.com>
2022-01-02 15:14:26 +0100

    SurveyVoteMessage model

--- ComeX.Lib.Common/ServerCommunicationModels/SurveyVoteMessage.cs
+++ ComeX.Lib.Common/ServerCommunicationModels/SurveyVoteMessage.cs
@@ -3,8 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace ComeX.Lib.Common.ServerCommunicationModels {
     class SurveyVoteMessage {
+        public SurveyVoteMessage(string token, List<Guid> answerId) {
+            Token = token;
+            AnswerId = answerId;
+        }
+
+        public SurveyVoteMessage() { }
+
+        [JsonProperty("Token")]
+        public string Token { get; set; }
+        [JsonProperty("AnswerId")]
+        public List<Guid> AnswerId { get; set; }
     }
 }
