b10a1087293098c15dc8e273e3b3dbb62a801818
konigin144 <kinga777s@gmail.com>
2022-01-02 17:28:02 +0100

    Load chat and survey - requests and responses

---
+++ ComeX.Lib.Common/ServerCommunicationModels/LoadSurveyRequest.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComeX.Lib.Common.ServerCommunicationModels {
+    public class LoadSurveyRequest {
+        public LoadSurveyRequest(string token, Guid roomId, string date) {
+            Token = token;
+            RoomId = roomId;
+            Date = date;
+        }
+
+        [JsonProperty("Token")]
+        public string Token { get; set; }
+        [JsonProperty("RoomId")]
+        public Guid RoomId { get; set; }
+        [JsonProperty("Date")]
+        public string Date { get; set; }
+    }
+}
