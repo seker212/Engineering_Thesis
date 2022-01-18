46a556e502ba7d18e6181679755c36341417b409
konigin144 <kinga777s@gmail.com>
2022-01-18 21:43:29 +0100

    SurveyVoteResponse & LoadSurveyVoteResponse

---
+++ ComeX.Lib.Common/ServerResponseModels/LoadSurveyVoteResponse.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComeX.Lib.Common.ServerResponseModels {
+    public class LoadSurveyVoteResponse {
+        public LoadSurveyVoteResponse(Guid roomId, List<SurveyVoteResponse> surveyVoteList) {
+            RoomId = roomId;
+            SurveyVoteList = surveyVoteList;
+        }
+
+        [JsonProperty("RoomId")]
+        public Guid RoomId { get; set; }
+        [JsonProperty("SurveyVoteList")]
+        public List<SurveyVoteResponse> SurveyVoteList { get; set; }
+    }
+}
