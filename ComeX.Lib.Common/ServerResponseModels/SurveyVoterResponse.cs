4a553d67abc0d02ce5fd0410e0a80878f166164c
konigin144 <kinga777s@gmail.com>
2022-01-18 12:41:24 +0100

    SurveyVoterResponse

---
+++ ComeX.Lib.Common/ServerResponseModels/SurveyVoterResponse.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComeX.Lib.Common.ServerResponseModels {
+    class SurveyVoterResponse {
+        public SurveyVoterResponse(Guid surveyId, Guid userId, bool voted) {
+            SurveyId = surveyId;
+            UserId = userId;
+            Voted = voted;
+        }
+
+        public SurveyVoterResponse() { }
+
+        [JsonProperty("SurveyId")]
+        public Guid SurveyId { get; set; }
+        [JsonProperty("UserId")]
+        public Guid UserId { get; set; }
+        [JsonProperty("Voted")]
+        public bool Voted { get; set; }
+    }
+}
